4f4c2216bf515cd6de07e0d8968bf5182d0f7a2a
Mariquita Villorante <mariquita.villorante@gmail.com>
2022-01-20 16:13:40 +0800

    Integrated Google Fonts API

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 
 function App(){
 	return(
-		<div className="app"> 
-			<header classname="app-header">
+		<div className="app">
+			<header className="app-header">
 			<h1>Qit's To-Do List</h1>
 			<p>Take 2, after 3 years</p>
 			</header>
