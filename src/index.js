c4d0bb26bd667bbef192b66eb476de61173acc04
Mariquita Villorante <mariquita.villorante@gmail.com>
2022-01-20 16:44:51 +0800

    created a new TasksList component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+//This is a function-based component and can be reused.
+function Taskslist(){
+	return(
+		<ul>
+				<li className="tasks-item">React Web Development</li>
+				<li className="tasks-item">Nihongo N4 Studies</li>
+				<li className="tasks-item">Test Automation</li>
+			</ul>
+		
+	)
+}
+
+
 function App(){
 	//an object used for styling
 	const paraStyle = {
 		"marginBottom":"15px"
 	};
+
 
 	return(
 		<div className="app">
@@ -14,11 +28,8 @@ function App(){
 			<h1>Qit's To-Do List</h1>
 			<p style={paraStyle}>Take 2, after 3 years</p>
 
-			<ul>
-				<li className="tasks-item">React Web Development</li>
-				<li className="tasks-item">Nihongo N4 Studies</li>
-				<li className="tasks-item">Test Automation</li>
-			</ul>
+			<Taskslist/>
+			
 			</header>
 		</div>
 	)
