5a2ea46e53eb7d2851e99755fc71df4092b6ec14
Mariquita Villorante <mariquita.villorante@gmail.com>
2022-01-20 16:31:15 +0800

    Added some CSS styles and implemented an inline style on first paragraph <p>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App(){
+	//an object used for styling
+	const paraStyle = {
+		"marginBottom":"15px"
+	};
+
 	return(
 		<div className="app">
 			<header className="app-header">
 			<h1>Qit's To-Do List</h1>
-			<p>Take 2, after 3 years</p>
+			<p style={paraStyle}>Take 2, after 3 years</p>
+
+			<ul>
+				<li className="tasks-item">React Web Development</li>
+				<li className="tasks-item">Nihongo N4 Studies</li>
+				<li className="tasks-item">Test Automation</li>
+			</ul>
 			</header>
 		</div>
 	)
